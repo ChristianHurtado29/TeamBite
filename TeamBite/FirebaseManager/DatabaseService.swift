303d4631f05635d01a6f989ba54c6f7d535f862b
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-04-21 01:48:48 -0400

    adding user auth verifier on database service

--- TeamBite/FirebaseManager/DatabaseService.swift
+++ TeamBite/FirebaseManager/DatabaseService.swift
@@ -7,3 +7,28 @@
 //
 
 import Foundation
+import FirebaseFirestore
+import FirebaseAuth
+
+class DatabaseService {
+    static let offeringsCollection = "offerings"
+    static let usersCollection = "users"
+    private let db = Firestore.firestore()
+    
+    private init() {}
+    static let shared = DatabaseService()
+    
+    
+    public func createUser(authDataResult: AuthDataResult, completion: @escaping (Result<Bool, Error>) -> ()){
+        guard let phone = authDataResult.user.phoneNumber else {
+            return
+        }
+        db.collection(DatabaseService.usersCollection).document(authDataResult.user.uid).setData(["phone":phone]){ (error) in
+            if let error = error {
+                completion(.failure(error))
+            } else {
+                completion(.success(true))
+            }
+        }
+    }
+}
