577cbaab04bc711421a0d334a8c3b6fac959c153
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-04-21 14:54:18 -0400

    added alert extension

--- TeamBite/Extensions/UIViewController + Alerts.swift
+++ TeamBite/Extensions/UIViewController + Alerts.swift
@@ -7,3 +7,13 @@
 //
 
 import Foundation
+import UIKit
+
+extension UIViewController {
+    public func showAlert(title: String?, message: String?) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alertController.addAction(okAction)
+        present(alertController, animated: true)
+    }
+}
