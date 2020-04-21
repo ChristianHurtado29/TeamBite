4f313058b23602ac842aa125f64c282c33d2ba63
ChristianHurtado29 <christianhurtado@pursuit.org>
2020-04-21 14:51:11 -0400

    added resources model

--- TeamBite/ViewControllers/ResourcesViewController.swift
+++ TeamBite/ViewControllers/ResourcesViewController.swift
@@ -13,7 +13,6 @@ class ResourcesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
