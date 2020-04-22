6a17a1a006e0cc126009df56174c710e99859a60
Margiett Gil <mxg627@icloud.com>
2020-04-22 12:36:39 -0400

    working on Detail View Controller

--- TeamBite/ViewControllers/UserDetailViewController.swift
+++ TeamBite/ViewControllers/UserDetailViewController.swift
@@ -12,16 +12,13 @@ import MapKit
 class UserDetailViewController: UIViewController {
     
     var selectedVenue: Venue!
+    var selectedOffer: Offer!
     let detailView = UserDetailView()
     private var annotation = MKPointAnnotation()
     private var isShowingNewAnnotation = false
     
     
-    private var detailVenues = [Venue]() {
-        didSet {
-        
-        }
-    }
+    private var detailVenues = [Venue]()
     
     override func loadView() {
         view = detailView
@@ -30,13 +27,31 @@ class UserDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        updateUI()
+        loadMap()
+        getDirections()
+        loadVenue()
+        
      
+    }
+    private func updateUI() {
+        detailView.restaurantInfo.text = ("\(selectedVenue.name.capitalized), Phone Number: \(selectedVenue.phoneNumber), Address: \(selectedVenue.address)")
+        detailView.hoursOFOperation.text = ("Start Time: \(selectedVenue.startTime), End Time: \(selectedVenue.endTime)")
+        detailView.numberOfMeals.text = ("Total of Meals: \(selectedOffer.totalMeals.description), Meals Remaining \(selectedOffer.remainingMeals)")
+        
+        // Have to add resturant picture !!
+        
     }
     
     private func loadMap() {
         let annotation = makeAnnotation(for: selectedVenue)
         detailView.locationMap.addAnnotation(annotation)
+        getDirections()
+    }
+    
+    private func loadVenue(){
+        
     }
     
     private func makeAnnotation(for venue: Venue) -> MKPointAnnotation {
@@ -52,10 +67,27 @@ class UserDetailViewController: UIViewController {
         return annotation
     }
     
+    private func getDirections() {
+        let coordinate = CLLocationCoordinate2D(latitude: selectedVenue.lat, longitude: selectedVenue.long)
+        let request = MKDirections.Request()
+        request.source = MKMapItem.forCurrentLocation()
+        request.destination = MKMapItem(placemark: MKPlacemark(coordinate: coordinate))
+        request.transportType = .any
+        let directions = MKDirections(request: request)
+        directions.calculate { (response, error) in
+            guard let unwrappedResponse = response else { request }
+            for route in unwrappedResponse.routes {
+                self.detailView.locationMap.addOverlay(route.polyline)
+                self.detailView.locationMap.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+            }
+        }
+    }
+    
 
 
 }
 
+//MARK: Extension for Mapkit
 extension UserDetailViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
         guard annotation is MKPointAnnotation else { return nil }
@@ -74,13 +106,13 @@ extension UserDetailViewController: MKMapViewDelegate {
         return annotationView
     }
     
-//    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKPolygonRenderer {
-//        let renderer = MKPolygonRenderer(polygon: overlay as! MKPolyline)
-//        renderer.strokeColor = UIColor.systemBlue
-//        renderer.lineWideth = 3.0
-//
-//        return renderer
-//    }
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKPolygonRenderer {
+        let renderer = MKPolygonRenderer(polygon: overlay as! MKPolyline)
+        renderer.strokeColor = UIColor.systemBlue
+        renderer.lineWideth = 3.0
+
+        return renderer
+    }
     
     func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
         if isShowingNewAnnotation {
