ed90c7efaeb196990b192c80deb9c61fac12491f
CameronRivera <cameronrivera@pursuit.org>
2020-04-22 18:23:20 -0400

    Added more to the loginFlow

---
+++ TeamBite/Views/UserFoodPreferencesView.swift
@@ -0,0 +1,20 @@
+//
+//  UserAllergiesView.swift
+//  TeamBite
+//
+//  Created by Cameron Rivera on 4/22/20.
+//  Copyright © 2020 Christian Hurtado. All rights reserved.
+//
+
+import UIKit
+
+class UserFoodPreferencesView: UIView {
+    
+    public lazy var directionsLabel: UILabel = {
+       let label = UILabel()
+        return label
+    }()
+    
+    public lazy var 
+
+}
