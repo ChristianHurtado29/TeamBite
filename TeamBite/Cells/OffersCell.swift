8f5b47765171826ba5d9419b0f2000482e2cccdd
David <lin.david985@gmail.com>
2020-04-22 18:25:16 -0400

    updated create offers vc

--- TeamBite/Cells/OffersCell.swift
+++ TeamBite/Cells/OffersCell.swift
@@ -12,4 +12,13 @@ class OffersCell: UITableViewCell {
     @IBOutlet weak var offerName: UILabel!
     @IBOutlet weak var totalNumOfMeals: UILabel!
     @IBOutlet weak var totalNumMealsLeft: UILabel!
+    
+    
+    public func configureCell(for offer: Offer) {
+        offerName.text = offer.nameOfOffer
+        totalNumOfMeals.text = offer.totalMeals.description
+        totalNumMealsLeft.text = offer.remainingMeals.description
+    }
+
+    
 }
--- TeamBite/ViewControllers/CreateOffersViewController.swift
+++ TeamBite/ViewControllers/CreateOffersViewController.swift
@@ -13,15 +13,88 @@ class CreateOffersViewController: UIViewController {
     @IBOutlet weak var numberOfMealsTextField: UITextField!
     @IBOutlet weak var startTimeDatePicker: UIDatePicker!
     @IBOutlet weak var endTimeDatePicker: UIDatePicker!
+    @IBOutlet weak var createButton: UIButton!
+    
+    //Allergies switch
+    @IBOutlet weak var nutFreeSwitch: UISwitch!
+    @IBOutlet weak var glutenFreeSwitch: UISwitch!
+    @IBOutlet weak var vegetarianSwitch: UISwitch!
+    
+    let currentDateTime = Date()
+    
+    var allergies = [String]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initialSwitchSettings()
+        numberOfMealsTextField.keyboardType = .numberPad
+        createButton.isEnabled = false
     }
+    
+    
+    private func initialSwitchSettings() {
+        nutFreeSwitch.isOn = false
+        glutenFreeSwitch.isOn = false
+        vegetarianSwitch.isOn = false
+    }
+    
+    @IBAction func nutFreeSwitchPressed(_ sender: UISwitch) {
+        
+    }
+    
+    @IBAction func glutenFreeSwitchPressed(_ sender: UISwitch) {
+        
+    }
+    
+    @IBAction func vegetarainSwitchPressed(_ sender: UISwitch) {
+        
+    }
+    
     
     @IBAction func createOfferButtonPressed(_ sender: UIButton) {
+        if offerNameTextField.text?.isEmpty == false {
+            sender.isEnabled = true
+        }
+        
+        if nutFreeSwitch.isOn == true {
+            allergies.append("Nut-Free")
+        }
+        if glutenFreeSwitch.isOn == true {
+            allergies.append("Gluten Free")
+        }
+        if vegetarianSwitch.isOn == true {
+            allergies.append("Vegetarian")
+        }
+        
+        if startTimeDatePicker.date < currentDateTime || endTimeDatePicker.date < startTimeDatePicker.date {
+            showAlert(title: "", message: "Please provide a valid start time")
+        }
+        
+        let offerName = offerNameTextField.text ?? "Meals"
+        let numberOfMeals = Int(numberOfMealsTextField.text ?? "0")
+        let startTime = startTimeDatePicker.date
+        let endTime = endTimeDatePicker.date
+        let finalAllergies = allergies
         
         
+        let newOffer = Offer(offerId: UUID().uuidString , nameOfOffer: offerName, totalMeals: numberOfMeals!, remainingMeals: numberOfMeals!, createdDate: Date(), startTime: startTime, endTime: endTime, allergyType: finalAllergies)
         
+        
+        DatabaseService.shared.addToOffers(offer: newOffer) { [weak self, weak sender] (result) in
+            switch result {
+            case.failure(let error):
+              DispatchQueue.main.async {
+                self?.showAlert(title: "Error creating item", message: "Sorry something went wrong: \(error.localizedDescription)")
+                sender?.isEnabled = true
+              }
+            case .success(let offerData):
+                self?.upload
+              sender?.isEnabled = true
+                
+            }
+        
+        }
         
         
         
@@ -32,5 +105,5 @@ class CreateOffersViewController: UIViewController {
     
     
     
-
+    
 }
