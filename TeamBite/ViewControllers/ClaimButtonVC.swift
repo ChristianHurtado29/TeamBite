4305143fdc34f56c06779a3add0bef5b38acfd5a
Margiett Gil <mxg627@icloud.com>
2020-04-22 23:55:28 -0400

    working on the claim button meal

--- TeamBite/ViewControllers/ClaimButtonVC.swift
+++ TeamBite/ViewControllers/ClaimButtonVC.swift
@@ -19,11 +19,27 @@ class ClaimButtonVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //view.backgroundColor = .systemBackground
+        view.backgroundColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1) // testing
+        navigationItem.title = "Claim Your Meal"
 
         
     }
     
-
+    @objc private func oneMeal(_ sender: UIButton) {
+        print("Selected one meal")
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let continueAction = UIAlertAction(title: "Claim My Meal", style: .default)
+        }
     
+    @objc private func twoMeal(_ sender: UIButton) {
+        print("Selected Two Meals")
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        }
+    }
 
-}
+        }
+        
